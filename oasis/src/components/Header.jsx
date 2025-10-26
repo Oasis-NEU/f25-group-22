3d5723120515309adf8101d08ed2364e8041066e
Sean Behan <behan.s@northeastern.edu>
2025-10-26 14:10:25 -0400

    working on header and home

--- oasis/src/components/Header.jsx
+++ oasis/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router";
 
+
+
 export default function Header() {
   return (
     <header className="bg-white shadow-sm">
@@ -8,22 +10,30 @@ export default function Header() {
         <div className="flex justify-between items-center h-16">
           <Link to="/">
             <div className="flex items-center">
-              <span style={{ color: "#2E6F40" }} className="text-3xl font-bold">
-                Oasis Project
+              <span style={{ color: "#2E6F40",
+                fontFamily: "'Cabin', sans-serif",
+                fontSize: "30px",
+                fontWeight: "bold"
+              }}>
+                RouteReady
               </span>
             </div>
           </Link>
 
           <div className="flex items-center gap-3">
             <Link to={"/login"}>
-              <button className="px-4 py-2 text-gray-700 hover:text-green-800 font-medium transition-colors cursor-pointer">
+              <button 
+              style = {{fontFamily: "'Cabin', sans-serif"}}
+              className="px-4 py-2 text-gray-700 hover:text-green-800 font-medium transition-colors cursor-pointer">
                 Log In
               </button>
             </Link>
 
             <Link to="signup">
               <button
-                style={{ backgroundColor: "#2E6F40" }}
+                style={{ backgroundColor: "#2E6F40",
+                  fontFamily: "'Cabin', sans-serif"
+                }}
                 className="px-5 py-2 text-white rounded-lg font-medium transition-colors shadow-sm cursor-pointer"
               >
                 Sign Up
