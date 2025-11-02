01d15d09cfc8a6c5da22cbdd54201ebe06c42fc5
zoheb-akhtar <akhtar.zo@northeastern.edu>
2025-11-02 12:20:46 -0500

    Added supabase client

--- server/controllers/trailsController.js
+++ server/controllers/trailsController.js
@@ -2,12 +2,6 @@ import pool from "../config/db";
 
 export const getAllTrails = async (req, res) => {
   try {
-    const trails = await pool.query("SELECT * FROM trails");
-    if (!trails || trails.rows.length === 0) {
-      res.status(200).json({ message: "No trails found" });
-    }
-
-    res.status(200).json({ trails: trails.rows });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
